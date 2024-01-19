141842519f8d8ebf1f381e6a5ed9d17d3d6ce7c2
Arya Patel <97847987+Arya-P05@users.noreply.github.com>
2024-01-19 15:50:26 -0500

    Initial commit from Create Next App

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+// pages/index.js
+import React from 'react';
+import { Header } from '../components';
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <p>Content of your home page</p>
+    </div>
+  );
+};
+
+export default Home;
